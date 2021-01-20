34c3a1e22692d648022e2139912f028405a5710a
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-01-20 11:52:03 -0300

    Payment, one-to-one association

--- src/main/java/com/dhyegopedroso/course/config/TestConfig.java
+++ src/main/java/com/dhyegopedroso/course/config/TestConfig.java
@@ -16,6 +16,7 @@ import com.dhyegopedroso.course.Repositories.UserRepository;
 import com.dhyegopedroso.course.entities.Category;
 import com.dhyegopedroso.course.entities.Order;
 import com.dhyegopedroso.course.entities.OrderItem;
+import com.dhyegopedroso.course.entities.Payment;
 import com.dhyegopedroso.course.entities.Product;
 import com.dhyegopedroso.course.entities.User;
 import com.dhyegopedroso.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 		
 
 	}
